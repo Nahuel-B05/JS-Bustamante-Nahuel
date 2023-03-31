7807350cb3e604a2be4789905f49c97207a6ae38
Nahuel-B05 <nahuelbus2@gmail.com>
2023-03-31 17:33:06 -0300

    se actualizo

--- java.js
+++ java.js
@@ -1,22 +1,45 @@
 
-const productos = [
-        { id: 1, nombre: 'Boku No Hero A.', precio: 1000, vol: 33, img: 'https://infoliteraria.com/wp-content/uploads/2022/01/myhero_academia.jpg.webp' },
-        { id: 2, nombre: 'One Piece', precio: 1000, vol: 100, img: 'https://images.squarespace-cdn.com/content/v1/571abd61e3214001fb3b9966/1b0f0c7b-7b0c-412a-8de3-f131c1e07f94/One+Piece+100.jpg' },
-        { id: 3, nombre: 'Naruto', precio: 1000, vol: 71, img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61VMDNFOUAL._AC_UF1000,1000_QL80_.jpg' },
-        { id: 4, nombre: 'Tokyo Ghoul', precio: 1000, vol: 11, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUu4qepXj53PcYVQQNHw2jcqxx6C6J4g7y5b-6atjrkNE_0F7eIJr8IIZnX3jrBZrlUOY&usqp=CAU' },
-        { id: 5, nombre: 'Slam Dunk', precio: 1000, vol: 24, img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71vc7vW+pJL.jpg' },
-        { id: 6, nombre: 'Dragon Ball S.', precio: 1000, vol: 15, img: 'https://www.tematika.com/media/catalog/Ilhsa/Imagenes/690909.jpg' },
-        { id: 7, nombre: 'Demon Slayer', precio: 1000, vol: 10, img: 'https://dwgkfo5b3odmw.cloudfront.net/manga/social/share-19646-DemonSlayer_GN10_Web_jpg' },
-        { id: 8, nombre: 'shingeky no k.', precio: 1000, vol: 32, img: 'https://www.tierragamer.com/wp-content/uploads/2020/11/SNK-a-color.png' },
-        { id: 9, nombre: 'berserk', precio: 1000, vol: 6, img: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/001/749/463/products/82018bcc-4cbe-0efc-37d8-19042ec4a1201-92935c79c850c9593d16513381450979-640-0.jpg' },
-        { id: 10, nombre: 'h x H', precio: 1000, vol: 7, img: 'https://i.pinimg.com/originals/d6/57/d9/d657d9f8cc0240a952f2a3982075ea84.jpg' }
-]
-let carrito = [];
-let stringReducido = 0;
-let strinMenos = 0;
-function crearProductos() {
-        for (const producto of productos) {
-                let { id, nombre, precio, vol, img } = producto
+// const productos = [
+//         { id: 1, nombre: 'Boku No Hero A.', precio: 1000, vol: 33, img: 'https://infoliteraria.com/wp-content/uploads/2022/01/myhero_academia.jpg.webp' },
+//         { id: 2, nombre: 'One Piece', precio: 1000, vol: 100, img: 'https://images.squarespace-cdn.com/content/v1/571abd61e3214001fb3b9966/1b0f0c7b-7b0c-412a-8de3-f131c1e07f94/One+Piece+100.jpg' },
+//         { id: 3, nombre: 'Naruto', precio: 1000, vol: 71, img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61VMDNFOUAL._AC_UF1000,1000_QL80_.jpg' },
+//         { id: 4, nombre: 'Tokyo Ghoul', precio: 1000, vol: 11, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUu4qepXj53PcYVQQNHw2jcqxx6C6J4g7y5b-6atjrkNE_0F7eIJr8IIZnX3jrBZrlUOY&usqp=CAU' },
+//         { id: 5, nombre: 'Slam Dunk', precio: 1000, vol: 24, img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71vc7vW+pJL.jpg' },
+//         { id: 6, nombre: 'Dragon Ball S.', precio: 1000, vol: 15, img: 'https://www.tematika.com/media/catalog/Ilhsa/Imagenes/690909.jpg' },
+//         { id: 7, nombre: 'Demon Slayer', precio: 1000, vol: 10, img: 'https://dwgkfo5b3odmw.cloudfront.net/manga/social/share-19646-DemonSlayer_GN10_Web_jpg' },
+//         { id: 8, nombre: 'shingeky no k.', precio: 1000, vol: 32, img: 'https://www.tierragamer.com/wp-content/uploads/2020/11/SNK-a-color.png' },
+//         { id: 9, nombre: 'berserk', precio: 1000, vol: 6, img: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/001/749/463/products/82018bcc-4cbe-0efc-37d8-19042ec4a1201-92935c79c850c9593d16513381450979-640-0.jpg' },
+//         { id: 10, nombre: 'h x H', precio: 1000, vol: 7, img: 'https://i.pinimg.com/originals/d6/57/d9/d657d9f8cc0240a952f2a3982075ea84.jpg' }
+// ]
+
+// function crearProductos() {
+//         for (const producto of productos) {
+//                 let { id, nombre, precio, vol, img } = producto
+//                 let cardManga = document.createElement('div');
+//                 cardManga.innerHTML = `
+//                 <img src="${img}"alt="Manga">
+//                 <h2>${nombre}</h2>
+//                 <h3>$ ${precio}</h3>
+//                 <a href:"#" id=${id}>Add To Cart</a>
+//                 <div class="numero-manga">
+//                 <h3>#${vol}</h3>
+//                 </div>
+//                 `
+//                 let contenedor = document.getElementById('productos')
+//                 cardManga.className = 'cardManga'
+//                 contenedor.append(cardManga)
+//                 botonClick(id, carritoAdd)
+//         }
+// }
+
+
+const contenedor = document.getElementById('productos')
+fetch('./data.json')
+        .then((res)=>res.json())
+        .then ((data)=> {
+                data.forEach((producto)=>{
+                let {nombre, id, vol, precio, img} = producto
+                productos.push(producto)
                 let cardManga = document.createElement('div');
                 cardManga.innerHTML = `
                 <img src="${img}"alt="Manga">
@@ -31,8 +54,14 @@ function crearProductos() {
                 cardManga.className = 'cardManga'
                 contenedor.append(cardManga)
                 botonClick(id, carritoAdd)
-        }
-}
+                })
+        })
+        // .catch((err) => console.error(`No se cargo correctamente los datos`));
+let productos =[]
+let carrito = [];
+let stringReducido = 0;
+let strinMenos = 0;
+
 
 function botonClick(id, funcion) {
         let botonAdd = document.getElementById(id)
@@ -68,6 +97,7 @@ function restarIndiceBoton(e) {
                 carrito.splice(indiceManga, 1)
                 borrar.remove();
                 localS()
+                borrarboton()
         } else {
                 let cantidad = carrito[indiceManga].cantidad -= 1
                 renderExistente(cantidad, stringReducido)
@@ -156,7 +186,7 @@ function renderBotonVaciar() {
         }
 }
 function vaciarCarro() {
-        carrito = [];
+        carrito=[]
         localStorage.clear()
         let boxes = document.querySelectorAll('.cardCarrito');
         boxes.forEach(box => {
@@ -164,8 +194,8 @@ function vaciarCarro() {
         });
         let boton = document.getElementById('vaciarCarro');
         boton.remove()
-
 }
+
 if (localStorage.getItem('carrito')) {
         let carroLocS = localStorage.getItem('carrito')
         carroLocS = JSON.parse(carroLocS)
@@ -175,4 +205,8 @@ if (localStorage.getItem('carrito')) {
                 render(nombre, vol, id, precio, img, cantidad)
         }
 }
-crearProductos()
+function borrarboton (){  
+        if(carrito==0){
+        document.getElementById('vaciarCarro').remove()}
+}
+// crearProductos()
